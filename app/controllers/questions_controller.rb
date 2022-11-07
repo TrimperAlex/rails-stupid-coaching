cb0b515700e366f110a08081bba9ee82ec8c45a4
Alex Trimper <trimper.a@gmail.com>
2022-11-07 15:23:46 -0400

    Completed stupid coaching challenge

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,16 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @ask = params[:question]
+
+    if @ask.end_with?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @ask == 'I am going to work right now!'
+      @answer = 'Great!'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
